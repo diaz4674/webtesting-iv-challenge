311ac25dc3dd9be4a243d2cc7e053daa5df82f49
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-06-27 21:34:31 -0700

    added created/delete helpers

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const heros = require('../heros/herosModel')
+const Heros = require('../heros/herosModel')
 
 const server = express()
 
@@ -8,5 +9,38 @@ server.use(express.json())
 server.get('/', (req, res) => {
     res.status(200).json('hello')
 })
+
+server.get('/heros', async(req, res)=> {
+    Heros.getAll() 
+        .then(heros => {
+            res.status(200).json(heros)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+server.post('/heros', async(req, res) => {
+    Heros.add(req.body)
+        .then(newHero =>{
+            res.status(200).json(newHero)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+server.delete('/heros/:id', async(req, res) => {
+    const {id} = req.params
+    Heros.remove(id)
+    .then(delted => {
+        res.status(200).json(delted)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+
 
 module.exports = server
--- heros/herosModel.js
+++ heros/herosModel.js
@@ -1,20 +1,31 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-  insert,
+  add,
+  update,
   remove,
   getAll,
+  findById,
 };
 
-async function insert(hero) {
+async function add(hero) {
   const [id] = await db('heros').insert(hero);
   return findById(id)
 }
 
-function remove(id) {
+async function update(id, changes) {
   return null;
+}
+
+async function remove(id) {
+   const deletedHero = await db('heros').where({id}).del();
+  return findById(deletedHero)
 }
 
 function getAll() {
   return db('heros');
+}
+
+function findById(id) {
+  return db('heros').where({id}).first()
 }
